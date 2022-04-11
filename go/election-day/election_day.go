6d57dc77f93f8988d2fa7c508b5772fd59f6c61e
Alejandro Diaz <alejandro.diaz@axians.es>
2022-04-11 08:54:17 +0200

    Add election-day exercise

--- go/election-day/election_day.go
+++ go/election-day/election_day.go
@@ -1,5 +1,7 @@
 package electionday
 
+import "strconv"
+
 // NewVoteCounter returns a new vote counter with
 // a given number of inital votes.
 func NewVoteCounter(initialVotes int) *int {
@@ -25,15 +27,25 @@ func IncrementVoteCount(counter *int, increment int) {
 
 // NewElectionResult creates a new election result
 func NewElectionResult(candidateName string, votes int) *ElectionResult {
-	panic("Please implement the NewElectionResult() function")
+	var elect ElectionResult
+	elect.Name = candidateName
+	elect.Votes = votes
+	return &elect
+
 }
 
 // DisplayResult creates a message with the result to be displayed
 func DisplayResult(result *ElectionResult) string {
-	panic("Please implement the DisplayResult() function")
+	elected := *result
+	display := elected.Name + " (" + strconv.Itoa(elected.Votes) + ")"
+	return display
+
 }
 
 // DecrementVotesOfCandidate decrements by one the vote count of a candidate in a map
 func DecrementVotesOfCandidate(results map[string]int, candidate string) {
-	panic("Please implement the DecrementVotesOfCandidate() function")
+	_, exists := results[candidate]
+	if exists {
+		results[candidate]--
+	}
 }
