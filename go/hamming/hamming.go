90159826f5903909a9c9d9285574e75ce4d1e403
Alejandro Diaz <alejandro.diaz@axians.es>
2022-04-12 15:53:38 +0200

    Add hamming exercise

---
+++ go/hamming/hamming.go
@@ -0,0 +1,16 @@
+package hamming
+
+import "errors"
+
+func Distance(a, b string) (int, error) {
+	count := 0
+	if len(a) != len(b) {
+		return -1, errors.New("must have same length")
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			count++
+		}
+	}
+	return count, nil
+}
